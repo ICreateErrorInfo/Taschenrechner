f1e2f733de49daea1c143cbc906d83e4d5787bfe
ICreateErrorInfo <hm.Github@smjh.de>
2021-09-15 13:32:22 +0200

    Add project files.

---
+++ Rechner/Term.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rechner
+{
+    class Term
+    {
+        public List<string> o = new List<string>();
+        public List<string> z = new List<string>();
+        public int pos = int.MaxValue;
+        public bool active = true;
+
+        public Term(List<string> o, List<string> z)
+        {
+            this.o = o;
+            this.z = z;
+        }
+        public Term()
+        {
+            
+        }
+    }
+}
